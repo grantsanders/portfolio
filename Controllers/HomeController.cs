64a89b264a0de37ad994e6b1690947947ec0247c
grantsanders <grantsanders89@gmail.com>
2022-11-03 15:42:13 -0400

    working background animation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,16 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Portfolio()
+    {
+        return View();
+    }
+
+    public IActionResult Resume()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {

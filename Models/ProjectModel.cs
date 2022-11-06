e7e55ccb0de68f5496f9494084ee3e4f966ad95a
grantsanders <grantsanders89@gmail.com>
2022-11-06 18:12:47 -0500

    intermediate- constructing models

---
+++ Models/ProjectModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace portfolio.Models;
+
+public class ProjectModel
+{
+    
+        public String Id { get; set; }
+
+        public String ProjectName { get; set; }
+
+        public String ProjectDescription { get; set; }
+
+    [JsonPropertyName ("img")]
+
+        public String Image { get; set; }
+
+        public String Url { get; set; }
+
+        
+        
+    }
+
+
